8e7168bcc5a19fbba9b75be5357923cb0de10f17
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-01 16:39:05 +0600

    single tesimonial

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -10,15 +10,38 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://process.env.DB_USER:process.env.DB_PASSWORD@cluster0.twhxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twhxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    console.log('POSDash DB Connected');
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const testimonialsCollection = client.db('posDash').collection('testimonials');
+
+        // testimonials
+        app.get('/testimonials', async (req, res) => {
+            const query = {};
+            const cursor = testimonialsCollection.find(query);
+            const testimonials = await cursor.toArray();
+
+            res.send(testimonials);
+        });
+
+        // single testimonial
+        app.get('testimonials/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const testimonial = await testimonialsCollection.findOne(query);
+
+            res.send(testimonial);
+        })
+    }
+    finally {
+
+    }
+};
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running POSDash Server');
