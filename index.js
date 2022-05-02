de2229e80ca083879cf0a718742d800c50cb6c8f
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-02 13:23:10 +0600

    403 error

--- index.js
+++ index.js
@@ -12,21 +12,20 @@ app.use(cors());
 app.use(express.json());
 
 // verify jwt
-const verifyJWT = (req, res, next) => {
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-
     if (!authHeader) {
         return res.status(401).send({ message: 'Unauthorized Access' });
     }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (err, decoded) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
             return res.status(403).send({ message: 'Forbidden Access' });
         }
+        res.header('Access-Control-Allow-Origin', '*');
         req.decoded = decoded;
+        next();
     });
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twhxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
