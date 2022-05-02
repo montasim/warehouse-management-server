528358404fc1118119b64750243179dda5df25ab
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-02 13:51:56 +0600

    removed all conflict error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -10,23 +9,6 @@ require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express.json());
-
-// // verify jwt
-// function verifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         return res.status(401).send({ message: 'Unauthorized Access' });
-//     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//         if (err) {
-//             return res.status(403).send({ message: 'Forbidden Access' });
-//         }
-//         res.header('Access-Control-Allow-Origin', '*');
-//         req.decoded = decoded;
-//         next();
-//     });
-// }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twhxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -116,8 +98,8 @@ async function run() {
 
         // display user items
         app.get('/my-items', async (req, res) => {
-            const decodedUserEmail = req.decoded.userEmail;
             const userEmail = req.query.userEmail;
+            console.log(userEmail);
             const query = { userEmail: userEmail };
             const cursor = myItemsCollection.find(query);
             const products = await cursor.toArray();
@@ -133,17 +115,9 @@ async function run() {
 
             res.send(result);
         });
-
-        // // JWT auth
-        // app.post('/login', async (req, res) => {
-        //     const user = req.body;
-        //     const accessToken = jwt.sign(user, process.env.JWT_ACCESS_TOKEN, {
-        //         expiresIn: '1d'
-        //     });
-        //     res.send(accessToken);
-        // });
     }
     finally {
+
     }
 };
 
